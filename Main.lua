be6546d4156c320b4437f13b049ad6670f45197b
evan4883 <114708992+evan4883@users.noreply.github.com>
2023-03-19 06:35:37 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,10 +1,9 @@
 local library = {}
 function library:CreateWindow()
-     _6hhf89yHS = Instance.new("ScreenGui")
-     Window = Instance.new("Frame")
-     UICorner = Instance.new("UICorner")
+	local _6hhf89yHS = Instance.new("ScreenGui")
+	local Window = Instance.new("Frame")
+	local UICorner = Instance.new("UICorner")
 	
-
 	_6hhf89yHS.Name = "6hhf89yHS"
 	_6hhf89yHS.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 	_6hhf89yHS.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
@@ -18,22 +17,4 @@ function library:CreateWindow()
 	UICorner.Parent = Window
 	UICorner.CornerRadius = UDim.new(0.100000001, 0)
 end
-local window = {}
-function window:CreateTap(Name)
-	local Tap1 = Instance.new("TextButton")
-	local UICorner_2 = Instance.new("UICorner")
-	Tap1.Name = "Tap1"
-	Tap1.Parent = Window
-	Tap1.BackgroundColor3 = Color3.new(0.313726, 0.313726, 0.313726)
-	Tap1.Position = UDim2.new(0, 0, 0.104347825, 0)
-	Tap1.Size = UDim2.new(0, 116, 0, 37)
-	Tap1.Font = Enum.Font.SourceSans
-	Tap1.Text = Name
-	Tap1.TextColor3 = Color3.new(0, 0, 0)
-	Tap1.TextSize = 14
-	
 
-	UICorner_2.Parent = Tap1
-	UICorner_2.CornerRadius = UDim.new(0.100000001, 0)
-	
-end
